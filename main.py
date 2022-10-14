4d742550301c3481b059873b45a5d5a221383583
taylornovara <taylornovara@gmail.com>
2022-10-14 12:12:58 -0500

    Changed logo to smaller file, updated labels, entries, buttons, changed some background colors

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ window.title("Locked | Password Manager")
 window.config(padx=20, pady=20, bg="white")
 
 # Setting window size
-window_width = 450
+window_width = 600
 window_height = 600
 
 # Get the screen dimension
@@ -23,29 +23,32 @@ window.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
 window.resizable(False, False)
 
 # Creates logo
-canvas = tkinter.Canvas(width=400, height=400)
+canvas = tkinter.Canvas(width=200, height=200, bg="white", highlightthickness=0)
 logo = tkinter.PhotoImage(file="locked.png")
-canvas.create_image(200, 200, image=logo)
+canvas.create_image(100, 100, image=logo)
 canvas.grid(row=0, column=0, columnspan=3)
 
+# Labels
 website_label = tkinter.Label(text="Website")
 website_label.grid(row=1, column=0)
-
-website_entry = tkinter.Entry()
-website_entry.grid(row=1, column=1)
 
 username_label = tkinter.Label(text="Username")
 username_label.grid(row=2, column=0)
 
-username_entry = tkinter.Entry()
-username_entry.grid(row=2, column=1)
+website_entry = tkinter.Entry()
+website_entry.grid(row=1, column=1)
 
 password_label = tkinter.Label(text="Password")
 password_label.grid(row=3, column=0)
 
+# Entries
+username_entry = tkinter.Entry()
+username_entry.grid(row=2, column=1)
+
 password_entry = tkinter.Entry()
 password_entry.grid(row=3, column=1)
 
+# Buttons
 generate_password_button = tkinter.Button(text="Generate Password")
 generate_password_button.grid(row=4, column=2)
 
