b56bbc4d8ddae473d1ce0e64edcfd34a2cd39372
taylornovara <taylornovara@gmail.com>
2022-10-16 08:38:27 -0500

    Changed logo, updated labels, entries, buttons, changed some background colors, refactored code

--- main.py
+++ main.py
@@ -3,58 +3,37 @@ import tkinter
 
 # Creates our window instance and sets title
 window = tkinter.Tk()
-window.title("Locked | Password Manager")
-window.config(pady=20, padx=20, bg="red")
+window.title("Password Manager")
+window.config(padx=50, pady=50)
 
-# Setting window size
-window_width = 600
-window_height = 400
-
-# Get the screen dimension
-screen_width = window.winfo_screenwidth()
-screen_height = window.winfo_screenheight()
-
-# Find the center point
-center_x = int(screen_width/2 - window_width / 2)
-center_y = int(screen_height/2 - window_height / 2)
-
-# Set the position of the window to the center of the screen, prevents user from changing window size
-window.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
-window.resizable(False, False)
-
-# Creates logo
-canvas = tkinter.Canvas(window, bg="white", highlightthickness=0)
-canvas.place(relx=0.5, rely=0.5, anchor="center")
-logo = tkinter.PhotoImage(file="locked.png")
-canvas.create_image(100, 100, image=logo)
-canvas.grid(row=0, column=0, columnspan=3)
+# Creates our logo
+canvas = tkinter.Canvas(height=200, width=200)
+logo_img = tkinter.PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=logo_img)
+canvas.grid(row=0, column=1)
 
 # Labels
-website_label = tkinter.Label(text="Website:", bg="white", fg='black')
+website_label = tkinter.Label(text="Website:")
 website_label.grid(row=1, column=0)
-
-username_label = tkinter.Label(text="Username:", bg="white", fg='black')
-username_label.grid(row=2, column=0)
-
-password_label = tkinter.Label(text="Password:", bg="white", fg='black')
+email_label = tkinter.Label(text="Email:")
+email_label.grid(row=2, column=0)
+password_label = tkinter.Label(text="Password:")
 password_label.grid(row=3, column=0)
 
 # Entries
-website_entry = tkinter.Entry(width=35, bg="white", highlightthickness=0)
+website_entry = tkinter.Entry(width=37)
 website_entry.grid(row=1, column=1, columnspan=2)
-
-username_entry = tkinter.Entry(width=35, bg="white", highlightthickness=0)
-username_entry.grid(row=2, column=1, columnspan=2)
-
-password_entry = tkinter.Entry(width=35, bg="white", highlightthickness=0)
-password_entry.grid(row=3, column=1, columnspan=2)
+website_entry.focus()
+email_entry = tkinter.Entry(width=37)
+email_entry.grid(row=2, column=1, columnspan=2)
+password_entry = tkinter.Entry(width=21)
+password_entry.grid(row=3, column=1)
 
 # Buttons
-generate_password_button = tkinter.Button(text="Generate Password", width=10, bg="white", highlightthickness=0)
-generate_password_button.grid(row=4, column=2)
-
-add_button = tkinter.Button(text="Add", width=10, bg="white", highlightthickness=0)
-add_button.grid(row=4, column=1)
+generate_password_button = tkinter.Button(text="Generate Password", width=11)
+generate_password_button.grid(row=3, column=2)
+add_button = tkinter.Button(text="Add", width=35)
+add_button.grid(row=4, column=1, columnspan=2)
 
 # Keeps program window open
 window.mainloop()
